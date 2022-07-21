000f172b84891352577575567ff4e250911cb245
Christian F. Peres <christ.peres@gmail.com>
2022-07-21 09:54:04 -0300

    First commit

---
+++ src/shared/http/routes/index.ts
@@ -0,0 +1,12 @@
+import productsRouter from '@modules/products/routes/productsRouter';
+import { Router } from 'express';
+
+const routes = Router();
+
+routes.use('/products', productsRouter);
+
+routes.get('/', (request, response) => {
+  return response.json({ message: 'Hello Dev' });
+});
+
+export default routes;
