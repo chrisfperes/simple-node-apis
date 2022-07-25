379829614a4c7b7ea8de442d7fcc0c73b0c17462
Christian F. Peres <christ.peres@gmail.com>
2022-07-25 16:43:58 -0300

    Another commit

---
+++ src/modules/users/typeorm/repositories/UserTokensRepository.ts
@@ -0,0 +1,21 @@
+import { EntityRepository, Repository } from 'typeorm';
+import UserToken from '../entities/UserToken';
+
+@EntityRepository(UserToken)
+export class UserTokensRepository extends Repository<UserToken> {
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    return this.findOne({
+      where: {
+        token,
+      },
+    });
+  }
+
+  public async generate(user_id: string): Promise<UserToken | undefined> {
+    return this.save(
+      this.create({
+        user_id,
+      }),
+    );
+  }
+}
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,4 +1,5 @@
 import productsRouter from '@modules/products/routes/productsRouter';
+import passwordRouter from '@modules/users/routes/passwordRouter';
 import sessionRouter from '@modules/users/routes/sessionRouter';
 import usersRouter from '@modules/users/routes/usersRouter';
 
@@ -9,5 +10,6 @@ const routes = Router();
 routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
